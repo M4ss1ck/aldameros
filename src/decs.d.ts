4b3659f16d27199b1c4962210fbaaadd3b6016da
M4ss1ck <4ndyraul@gmail.com>
2022-05-25 23:03:52 -0400

    remove unnecesary types

--- src/decs.d.ts
+++ src/decs.d.ts
@@ -1,5 +1,3 @@
-declare module "react-to-pdf"
-declare module "gatsby-plugin-dark-mode"
 declare module "*.svg" {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>
   export default content
